2e987e8aa269e9cb5da97455d67a31487492d441
Maxbeurer <haaandrew.feng@gmail.com>
2025-02-12 17:42:39 +0100

    Changed background's color

--- js/P2_EscenaAnimada.js
+++ js/P2_EscenaAnimada.js
@@ -58,7 +58,7 @@ function init()
     document.getElementById('container').appendChild( renderer.domElement );
     // Escena
     scene = new THREE.Scene();
-    
+    scene.background = new THREE.Color(0.5,0.5,0.5);
     // Camara
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1,1000);
     camera.position.set( 0.5, 2, 7 );
